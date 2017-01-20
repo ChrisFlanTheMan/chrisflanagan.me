d060a21ca00139c9c6175ca56a04b5b2bdaaebfe
Chris Flanagan <crflanag@gmail.com>
2017-01-19 21:09:18 -0500

    New landing page + new green colouring

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,9 +10,15 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response){
+router.get('/', function(request, response){
+      response.render('home.ejs');
+});
+
+router.get('/resume', function(request, response){
       response.render('resume.ejs');
 });
+
+app.use('/', router);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
