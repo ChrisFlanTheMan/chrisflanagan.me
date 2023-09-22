fe3a3ce335d7e86001538b733d979c6c1088eed6
Chris Flanagan <crflanag@gmail.com>
2023-09-22 14:16:05 -0400

    Update node modules

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 4000));
 
 app.use(express.static(__dirname + '/public'));
 
